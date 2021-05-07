39c44858e917f31fcfdb788a2bfa7887deba6a4a
Kingsley Bawuah <Kbawuah@caring.com>
2021-05-07 14:39:47 -0400

    reading port number from env, testing heroku.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/KingsleyBawuah/fs-challenge
 
 go 1.15
 
-require github.com/google/go-github/v35 v35.2.0
+require (
+	github.com/google/go-github/v35 v35.2.0
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "github.com/google/go-github/v35/github"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/google/go-github/v35/github"
+	"github.com/joho/godotenv"
+)
+
+func handleNote(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "handleNote\n")
+}
 
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		//TODO: DO not panic.
+		log.Panic(err)
+	}
+	port := os.Getenv("PORT")
+	fmt.Println("PORT is", port)
+
+	http.HandleFunc("/handleNote", handleNote)
+
+	//TODO: Figure out how to read env for port number.
+	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+
 	_ = github.NewClient(nil)
 }
